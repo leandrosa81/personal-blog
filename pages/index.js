f4248abbd9e9e148cbfc4a83fa5d3f733163f975
leandro <leandrosa81@gmail.com>
2018-07-26 16:43:08 -0300

    about page, and image on the front page

--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,25 @@ import React from 'react'
 import { Link } from 'react-router'
 import { prefixLink } from 'gatsby-helpers'
 import Helmet from "react-helmet"
-//import { config } from 'config'
+import { config } from 'config'
 
 export default class Index extends React.Component {
   render () {
     return (
       <div>
         <Helmet
-          title={'lugarinho.tech'}
+          title={config.siteTitle}
           meta={[
-            {"name": "description", "content": "Sample"},
-            {"name": "keywords", "content": "sample, something"},
+            {"name": "description", "content": "lugarinho.tech"},
+            {"name": "keywords", "content": "technology, development, software, blog, lugarinho"},
           ]}
         />
         <h1>
-          soon.
+          Hi :)
         </h1>
-        <p>:)</p>
+        <p>Welcome to my blog. I plan to share some articles about software development.</p>
+        <img src="https://source.unsplash.com/random/800x200" width='100%' alt="" />
+        <small><Link to="/about/">about me</Link></small>
       </div>
     )
   }
