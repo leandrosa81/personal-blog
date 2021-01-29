061fef4cbb157dd4acd224fd3859ef8c9cdd9f44
leandro <leandrosa81@gmail.com>
2021-01-29 12:43:31 -0300

    fixed image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,14 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import Template from "../components/template"
-
+import header from '../../assets/images/header.jpg'
 
 const IndexPage = () => {
   return (
 
     <Template>
-      <img src="https://source.unsplash.com/1200x200/?code,programming" width='100%' alt="" />
-
+      <img src={header} width='100%' alt="" />
+      
       <h1>
         Hi :)
         </h1>
@@ -16,7 +16,9 @@ const IndexPage = () => {
       <p>This is my <Link to="/blog/">blog</Link>.</p>
       <p>Curious? <Link to="/about/">Read more</Link> about me.</p>
       <p>This should be fun.</p>
-
+      <span style={{fontSize: "0.5em"}}>
+        Photo by <a href="https://unsplash.com/@boshkov?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Ilija Boshkov</a> on <a href="https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a>
+      </span>
     </Template>
   )
 }
