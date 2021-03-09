b1f01d5353551a614776ed787a9e8a5e5e94c4fa
leandro <leandrosa81@gmail.com>
2021-03-09 17:07:50 -0300

    add new taskbar-monitor version v0.3.0.

--- src/pages/tools/taskbar-monitor.js
+++ src/pages/tools/taskbar-monitor.js
@@ -25,44 +25,44 @@ const TaskbarMonitor = () => {
                 </ul>
                 <p>Currently it works well with the dark theme and horizontal taskbars, but I am working on it to support a wider taskbar config variety.</p>
                 <h2>Download</h2>
-                <p>Releases are <a href="https://github.com/leandrosa81/taskbar-monitor/releases" target="_blank" rel="noreferrer">available at Github.</a></p>
-                <p>Last version: 0.2.0</p>
+                <p>Releases are <a href="https://github.com/leandrosa81/taskbar-monitor/releases/latest" target="_blank" rel="noreferrer">available at Github.</a></p>
+                <p>Last version: 0.3.0</p>
                 <h2>Install instructions</h2>
                 
-                <p>To install the taskbar-monitor, you should download the zip file, extract it to a temp location and run the file TaskbarMonitorInstaller.exe.</p>
+                <p>To install the taskbar-monitor, first make sure you have <a href="https://dotnet.microsoft.com/download/dotnet-framework/net472" target="_blank" rel="noreferrer">.NET Framework 4.7.2 runtime</a> installed on your computer. If you use Windows 10 1803 April 2018 or later, it is already installed on your computer. Then, download the TaskbarMonitorInstaller.exe installer and run it.</p>
 
-                <p>It needs administrator rights to run, as it installs on you PROGRAM FILES (x86) folder.
-                The reason for this, is that this tool registers itself for all users, so the dll files should be accessible for
+                <p>It needs administrator rights to run, as it installs on your PROGRAM FILES (x86) folder.
+                The reason for this is that this tool registers itself for all users, so the dll files should be accessible for
                     all users on the computer.</p>
 
                 <h4>Manual install</h4>
 
-                <p>You can also manually install this tool, by putting the TaskbarMonitor.dll on any public location (eg. C:\taskbar-monitor) and
+                <p>If you compile it from the source, you can also manually install this tool by putting the TaskbarMonitor.dll on any public location (eg. C:\taskbar-monitor) and
                 running the following cmd (with admin privileges):
                         </p>
                 <pre>%SystemRoot%\Microsoft.NET\Framework64\v4.0.30319\regasm.exe /nologo /codebase "TaskbarMonitor.dll"</pre>
                 <h4>Uninstall</h4>
                 <p>To uninstall it, run the installer with "/uninstall" parameter</p>
                 <pre>TaskbarMonitorInstaller.exe /uninstall</pre>
+                <p>This will unregister the DLL and delete the files and folder on your PROGRAM FILES (x86) folder.</p>
                 <h4>Manual uninstall</h4>
                 <p>Run the following cmd:</p>
                 <pre>%SystemRoot%\Microsoft.NET\Framework64\v4.0.30319\regasm.exe /unregister "TaskbarMonitor.dll"</pre>
-                <p>Then, after a reboot, delete the folder "TaskbarMonitor" on your PROGRAM FILES (x86) folder.</p>
+                <p>Then, after a reboot, delete the files.</p>
 
 
                 <h2>Usage</h2>
-                <p>To activate it, you must select it on the taskbar context menu.</p>
+                <p>To activate it, you must select it on the taskbar context menu. Right click on your tasbak, go to "Taskbar", then select "Taskbar Monitor".</p>
                 <p>If you click with the right mouse button, a context menu will display several options:</p>
-                <img alt="second example" src={example2} />
-                <p>In the future it will be replaced with a proper "Settings" dialog window.</p>
+                <img alt="second example" src={example2} />                
 
                 <h2>Roadmap</h2>
                 <p>In the future, I plan to add some interesting features to this tool:</p>
                 <ul>
-                    <li>Settings dialog window.</li>
-                    <li>Save config on disk (per user, on appdata\programs folder)</li>
+                    <li style={{textDecoration: "line-through"}}>Settings dialog window.</li>
+                    <li style={{textDecoration: "line-through"}}>Save config on disk (per user, on appdata\programs folder)</li>
                     <li>theme selection</li>
-                    <li>full graph customization (size, labels, etc.)</li>
+                    <li style={{textDecoration: "line-through"}}>full graph customization (size, labels, etc.)</li>
                     <li>allow adding new graphs (plugins, eg. GPU, stock quotes)</li>
                 </ul>
 
